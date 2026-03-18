5798bd3e2ea58302c52fac8f0be729c118253065
lucas-aquila <lucas.aquila@shopee.com>
2026-03-17 21:15:24 -0300

    iniciando projeto

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.title("📊 Dashboard Volumetria")
+
+url = "https://docs.google.com/spreadsheets/d/SEU_ID/export?format=csv"
+
+df = pd.read_csv(url)
+
+st.dataframe(df)
+
+st.bar_chart(df["PROGRAMADO"])
